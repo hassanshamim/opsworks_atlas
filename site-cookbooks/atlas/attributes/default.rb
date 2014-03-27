3b92ef3a2ec35a48aa48a009bb5c1b8ef79c8fd1
Hassan Shamim <hassan.has.you@gmail.com>
2014-03-27 17:16:33 -0500

    delete the old ones

--- site-cookbooks/atlas/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-# install os packages
-# install the correct version of python 
-node['python']['install_method'] = 'source'
-node['python']['version'] = '3.4.0'
-node['python']['checksum'] =  'd2c83ea0217769a73e8b1ee33ffbca814903f8568e30f8d13e68e3d1f743449c'
